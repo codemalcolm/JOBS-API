b81aff1470acec1abaea4dd9b83ad1f2a38c6b22
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2025-03-08 21:01:40 +0100

    Initial controllers for jobs

---
+++ controllers/jobs.js
@@ -0,0 +1,26 @@
+const getAllJobs = async (req,res) =>{
+    res.send("getAllJobs")
+}
+
+const getJob = async (req,res) =>{
+    res.send("getJob")
+}
+
+const updateJob = async (req,res) =>{
+    res.send("updateJob")
+}
+
+const createJob = async (req,res) =>{
+    res.send("createJob")
+}
+
+const deleteJob = async (req,res) =>{
+    res.send("deleteJob")
+}
+
+module.exports = {getAllJobs,
+    getJob,
+    createJob,
+    updateJob,
+    deleteJob
+}

d59a31667344908ad76c1fb28339326780997be2
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2025-03-09 17:49:26 +0100

    Register controller with hashing

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,27 @@
 const express = require("express");
+const StatusCodes = require("http-status-codes");
+const User = require("../models/User");
+const { BadRequestError } = require("../errors");
+const bcrypt = require("bcryptjs");
 
-const register = (req, res) => {
-  res.send("REGISTER");
+const register = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    throw new BadRequestError("Credentials missing!");
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  // hashing password
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const tempUser = { name: name, email: email, password: hashedPassword };
+
+  const user = await User.create({ ...tempUser });
+  res.status(StatusCodes.CREATED).json({ msg: "ok", user });
 };
 
-const login = (req, res) => {
+const login = async (req, res) => {
   res.send("LOGIN");
 };
 

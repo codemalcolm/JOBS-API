554c19078d9c962abc8ba3cee69abc1a84f66530
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2025-03-09 17:41:18 +0100

    Setup db connection & changed api route name

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
-require('dotenv').config();
-require('express-async-errors');
-const express = require('express');
+require("dotenv").config();
+require("express-async-errors");
+const express = require("express");
 const app = express();
 
-const jobsRouter = require('./routes/jobs')
-const authRouter = require('./routes/auth')
+const jobsRouter = require("./routes/jobs");
+const authRouter = require("./routes/auth");
 
 // error handler
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+// DB connection
+const connectDB = require("./db/connect");
 
 app.use(express.json());
 // extra packages
 
 // routes
-app.get('/', (req, res) => {
-  res.send('jobs api');
+app.get("/", (req, res) => {
+  res.send("jobs api");
 });
 
-app.use('/jobs', jobsRouter);
-app.use('/auth', authRouter);
+app.use("/api/v1/jobs", jobsRouter);
+app.use("/api/v1/auth", authRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
@@ -28,8 +31,9 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
+      console.log(`Server is listening on port http://localhost:${port}...`)
     );
   } catch (error) {
     console.log(error);

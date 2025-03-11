92e41d7e726cf136239de603dda93d0fb7c10ae4
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2025-03-11 21:59:55 +0100

    Created login controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,25 +1,36 @@
 const express = require("express");
 const StatusCodes = require("http-status-codes");
 const User = require("../models/User");
-const { BadRequestError } = require("../errors");
+const { BadRequestError, UnauthenticatedError } = require("../errors");
 const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
   const { name, email, password } = req.body;
   if (!name || !email || !password) {
-    throw new BadRequestError("Credentials missing!");
+    throw new BadRequestError("Please provide name, email and password !");
   }
 
   const user = await User.create({ ...req.body });
-  
+
   // getting token from an instance method
   const token = user.createJWT();
-  
-  res.status(StatusCodes.CREATED).json({ user: {name : user.name}, token });
+
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
 const login = async (req, res) => {
-  res.send("LOGIN");
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new BadRequestError("Please provide email and password !");
+  }
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new UnauthenticatedError("Invalid Credentials");
+  }
+
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
 };
 
 module.exports = { login, register };

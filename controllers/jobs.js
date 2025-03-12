38fce9b3490ae31691ba6be4a23764dff833b030
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2025-03-12 15:59:12 +0100

    Created createJob controller

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,3 +1,7 @@
+const StatusCodes = require("http-status-codes");
+const Job = require("../models/Job");
+const { BadRequestError } = require("../errors");
+
 const getAllJobs = async (req, res) => {
   res.send("getAllJobs");
 };
@@ -11,7 +15,17 @@ const updateJob = async (req, res) => {
 };
 
 const createJob = async (req, res) => {
-  res.send("createJob");
+  const { company, position } = req.body;
+  // const createdBy = {res.user}
+  if (!company || !position) {
+    throw new BadRequestError("Please provide company and position");
+  }
+
+  req.body.createdBy = req.useruserId;
+
+  const job = await Job.create({ ...req.body });
+
+  res.status(StatusCodes.CREATED).json({ job });
 };
 
 const deleteJob = async (req, res) => {

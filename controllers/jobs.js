a73413c850c3a2887a669db70dc02b1b1f8394fc
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2025-03-12 19:05:35 +0100

    Created deleteJob controller & updateJob controller

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -4,29 +4,44 @@ const { BadRequestError, NotFoundError } = require("../errors");
 const ForbiddenError = require("../errors/forbidden");
 
 const getAllJobs = async (req, res) => {
-  const jobs = await Job.find({ createdBy: req.user.userId });
+  const jobs = await Job.find({ createdBy: req.user.userId }).sort("createdAt");
   res.status(StatusCodes.OK).json({ jobs });
 };
 
 const getJob = async (req, res) => {
   const { jobId } = req.params;
+  const userId = req.user.userId;
 
-  const job = await Job.findById(jobId);
+  const job = await Job.findOne({ _id: jobId, createdBy: userId });
+
   if (!job) {
     throw new NotFoundError(`Job with id: ${jobId} not found`);
-  }
-  console.log(req.user);
-
-  // user check
-  if (!job.checkUserAccess(req.user.userId)) {
-    throw new ForbiddenError("You are not allowed to view this document");
   }
 
   res.status(StatusCodes.OK).json({ job });
 };
 
 const updateJob = async (req, res) => {
-  res.send("updateJob");
+  const { jobId } = req.params;
+  const userId = req.user.userId;
+
+  const { company, position, state } = req.body;
+
+  if (!company && !position && !state) {
+    throw new BadRequestError("No changes made");
+  }
+
+  const job = await Job.findByIdAndUpdate(
+    { _id: jobId, createdBy: userId },
+    { ...req.body },
+    { runValidators: true, new: true }
+  );
+
+  if (!job) {
+    throw new NotFoundError(`Job with id: ${jobId} not found`);
+  }
+
+  res.status(StatusCodes.OK).json({ job });
 };
 
 const createJob = async (req, res) => {
@@ -44,7 +59,21 @@ const createJob = async (req, res) => {
 };
 
 const deleteJob = async (req, res) => {
-  res.send("deleteJob");
+  const userId = req.user.userId;
+  const { jobId } = req.params;
+
+  const deletedJob = await Job.findByIdAndDelete({
+    _id: jobId,
+    createdBy: userId,
+  });
+
+  if(!deletedJob){
+    throw new NotFoundError(`Job with id: ${jobId} not found`);
+  }
+
+  res
+    .status(StatusCodes.OK)
+    .json({ msg: "Successfully deleted document", deletedJob });
 };
 
 module.exports = { getAllJobs, getJob, createJob, updateJob, deleteJob };

7f4bf8333784e9ececb724c5fa99969336535bab
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2025-03-14 20:54:57 +0100

    Added security measures

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 require("dotenv").config();
 require("express-async-errors");
+
+// extra security
+const helmet = require("helmet");
+const cors = require("cors");
+const xs = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 const express = require("express");
 const app = express();
 
@@ -16,7 +23,18 @@ const connectDB = require("./db/connect");
 // middleware
 const authenticateUser = require("./middleware/authentication");
 
+app.set('trust proxy', 1)
+
+app.use(rateLimiter({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.);
+}))
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xs());
 // extra packages
 
 // routes
@@ -25,7 +43,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/jobs",authenticateUser, jobsRouter);
+app.use("/api/v1/jobs", authenticateUser, jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
